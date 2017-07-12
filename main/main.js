1f4543aa142bb915fd68e1de9d1aaebb7504f409
zlh <1261407236@qq.com>
2017-07-12 09:51:29 +0800

    00

--- main/main.js
+++ main/main.js
@@ -33,24 +33,3 @@ let receipItem=loadAllItems();
 function buildSingleItem(itemString) {
     return `商品编码:${itemString.barcode}，名称:${itemString.name}，数量:${itemString.count}${itemString.unit}，单价:${itemString.price.toFixed(2)}(元)，小计:${itemString.subTatal.toFixed(2)}(元)`;
 
-}
-function printReceipt() {
-    let itemStrings=processInput(inputs);
-    let itemtring="";
-    let total=0;
-    for(let k=0;k<itemStrings.length;k++){
-        if(k!=itemStrings.length-1){
-            itemtring+=buildSingleItem(itemStrings[k])+"\n";
-        }
-        else{
-            itemtring+=buildSingleItem(itemStrings[k]);
-        }
-        total+=itemStrings[k].subTatal;
-    }
-    console.log( `***<没钱赚商店>收据***
-${itemtring}
-----------------------
-总计：${total.toFixed(2)}(元)
-**********************`);
-}
-console.log(printReceipt());
